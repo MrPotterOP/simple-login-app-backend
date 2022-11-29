aded7ebbad19e5621698a1e8011e79a5ed4eaff5
Shubham Ubarhande <shubham.ubarhande1044@gmail.com>
2022-11-29 11:39:41 +0530

    fixed err

--- router/routes.js
+++ router/routes.js
@@ -15,7 +15,7 @@ const router = express.Router();
 router.post("/register", postRegister);
 router.post("/login", postLogin);
 router.get("/homepage", checkToken, getHomePage);
-router.test("/test", (req, res)=>{res.json({msg: "Server Running."})});
+router.get("/test", (req, res)=>{res.json({msg: "Server Running."})});
 
 
 export default router;
